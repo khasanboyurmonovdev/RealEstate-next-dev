0e4a43ec48d197b688d6dfcfb0203039208ff40a
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2026-02-24 14:39:48 +0900

    Backup before Uzbekistan rebuild

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { getLocaleMessages } from '../../libs/i18n';
 import useDeviceDetect from '../../libs/hooks/useDeviceDetect';
 import withLayoutBasic from '../../libs/components/layout/LayoutBasic';
 import { Stack, Box } from '@mui/material';
